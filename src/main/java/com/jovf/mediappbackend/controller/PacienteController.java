e2c62cb777549a20fb8dec6a6f1203d2bafc9195
oxalcvasquez <oxalc0101@gmail.com>
2024-03-31 10:32:54 -0500

    Status code in controller Paciente

--- src/main/java/com/jovf/mediappbackend/controller/PacienteController.java
+++ src/main/java/com/jovf/mediappbackend/controller/PacienteController.java
@@ -2,9 +2,14 @@ package com.jovf.mediappbackend.controller;
 
 import com.jovf.mediappbackend.model.Paciente;
 import com.jovf.mediappbackend.service.IPacienteService;
+import jakarta.servlet.Servlet;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -16,28 +21,41 @@ public class PacienteController {
 
 
     @GetMapping
-    public List<Paciente> listar() throws Exception {
-        return service.listar();
+    public ResponseEntity<List<Paciente>> listar() throws Exception {
+        List<Paciente> lista = service.listar();
+        return new ResponseEntity<>(lista, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public Paciente listar(@PathVariable Integer id) throws Exception {
-        return service.listarPorId(id);
+    public ResponseEntity<Paciente> listar(@PathVariable Integer id) throws Exception {
+        Paciente obj = service.listarPorId(id);
+        return new ResponseEntity<>(obj, HttpStatus.OK);
     }
 
+//    @PostMapping
+//    public ResponseEntity<Paciente> registrar(@RequestBody Paciente p) throws Exception {
+//        Paciente obj = service.registrar(p);
+//        return new ResponseEntity<>(obj, HttpStatus.CREATED);
+//    }
+
     @PostMapping
-    public Paciente registrar(@RequestBody Paciente p) throws Exception {
-        return service.registrar(p);
+    public ResponseEntity<Void> registrar(@RequestBody Paciente p) throws Exception {
+        Paciente obj = service.registrar(p);
+        // se quiere devolver la URL
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getIdPaciente()).toUri();
+        return ResponseEntity.created(location).build();
     }
 
     @PutMapping
-    public Paciente modificar(@RequestBody Paciente p) throws Exception {
-        return service.modificar(p);
+    public ResponseEntity<Paciente> modificar(@RequestBody Paciente p) throws Exception {
+        Paciente obj = service.registrar(p);
+        return new ResponseEntity<>(obj, HttpStatus.OK);
     }
 
     @DeleteMapping("{id}")
-    public void eliminar(@PathVariable Integer id) throws Exception {
+    public ResponseEntity<Void> eliminar(@PathVariable Integer id) throws Exception {
         service.eliminar(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
 
